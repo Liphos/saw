fb956e57e46bb52571e26460a1e02d4d41b4c690
Guillaume Damien Nicolas Levy <glevy@meta.com>
2026-09-03 20:38:20 +0200

    run: test metrics

--- impls/run_slurm.sh
+++ impls/run_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=saw_subgoal_sweep
-#SBATCH --output=logs/saw_subgoal_sweep_%A_%a.out
-#SBATCH --error=logs/saw_subgoal_sweep_%A_%a.err
-#SBATCH --array=0-139
+#SBATCH --job-name=test_metrics
+#SBATCH --output=logs/test_metrics_%A_%a.out
+#SBATCH --error=logs/test_metrics_%A_%a.err
+#SBATCH --array=0-79
 #SBATCH --time=08:00:00
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:volta:1
@@ -11,32 +11,47 @@
 unset SLURM_CPU_BIND
 
 SEEDS=(3917 3502 8948 9460 4729 2226 1744 7742 4501 6341)
-
+ALGOS=("hiql" "saw")
 ENVS=(
-    "humanoid-large-navigate-v0"
-    "humanoid-giant-navigate-v0"
+    "antmaze-giant-navigate-v0"
+    "antmaze-large-navigate-v0"
+    "cube-single-play-v0"
+    "cube-double-play-v0"
 )
 
-SUBGOAL_STEPS=(2 5 10 25 50 100 250)
-
+NUM_ALGOS=${#ALGOS[@]}
 NUM_ENVS=${#ENVS[@]}
-NUM_SUBGOAL_STEPS=${#SUBGOAL_STEPS[@]}
-
-SUBGOAL_IDX=$((SLURM_ARRAY_TASK_ID % NUM_SUBGOAL_STEPS))
-ENV_IDX=$(((SLURM_ARRAY_TASK_ID / NUM_SUBGOAL_STEPS) % NUM_ENVS))
-SEED_IDX=$((SLURM_ARRAY_TASK_ID / (NUM_SUBGOAL_STEPS * NUM_ENVS)))
+ALGO_IDX=$((SLURM_ARRAY_TASK_ID % NUM_ALGOS))
+ENV_IDX=$(((SLURM_ARRAY_TASK_ID / NUM_ALGOS) % NUM_ENVS))
+SEED_IDX=$((SLURM_ARRAY_TASK_ID / (NUM_ALGOS * NUM_ENVS)))
 
 SEED=${SEEDS[$SEED_IDX]}
+ALGO=${ALGOS[$ALGO_IDX]}
 ENV=${ENVS[$ENV_IDX]}
-KL_ALPHA=${KL_ALPHAS[$ENV_IDX]}
-SUBGOAL_STEP=${SUBGOAL_STEPS[$SUBGOAL_IDX]}
 
 source .venv/bin/activate
+
+EXTRA_ARGS=()
+
+case "$ENV" in
+    cube-single-play-v0)
+        EXTRA_ARGS+=(--agent.subgoal_steps=10)
+        if [ "$ALGO" = "saw" ]; then
+            EXTRA_ARGS+=(--agent.kl_alpha=0.3)
+        fi
+        ;;
+    cube-double-play-v0)
+        EXTRA_ARGS+=(--agent.subgoal_steps=10)
+        if [ "$ALGO" = "saw" ]; then
+            EXTRA_ARGS+=(--agent.kl_alpha=1.0)
+        fi
+        ;;
+esac
 
 srun python main.py \
     --env_name="$ENV" \
     --eval_episodes=50 \
-    --agent=agents/saw.py \
-    --agent.subgoal_steps="$SUBGOAL_STEP" \
+    --agent="agents/${ALGO}.py" \
     --seed="$SEED" \
-    --run_group="saw_subgoal_sweep"
+    --run_group="${ALGO}_test_metrics" \
+    "${EXTRA_ARGS[@]}"
