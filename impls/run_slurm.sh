860ed1599e19e78442255cd7fcf95d9953da4571
Guillaume Damien Nicolas Levy <glevy@meta.com>
2026-09-06 21:11:17 +0200

    run: new metrics

--- impls/run_slurm.sh
+++ impls/run_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=test_metrics
-#SBATCH --output=logs/test_metrics_%A_%a.out
-#SBATCH --error=logs/test_metrics_%A_%a.err
-#SBATCH --array=0-79
+#SBATCH --job-name=new_metrics
+#SBATCH --output=logs/new_metrics_%A_%a.out
+#SBATCH --error=logs/new_metrics_%A_%a.err
+#SBATCH --array=0-359  # 2 algos * 6 envs * 6 subgoal_steps * 5 seeds
 #SBATCH --time=08:00:00
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:volta:1
@@ -10,41 +10,48 @@
 
 unset SLURM_CPU_BIND
 
-SEEDS=(3917 3502 8948 9460 4729 2226 1744 7742 4501 6341)
+SEEDS=(3917 3502 8948 9460 4729)
 ALGOS=("hiql" "saw")
 ENVS=(
     "antmaze-giant-navigate-v0"
     "antmaze-large-navigate-v0"
     "cube-single-play-v0"
     "cube-double-play-v0"
+    "humanoidmaze-large-navigate-v0"
+    "humanoidmaze-giant-navigate-v0"
 )
+SUBGOAL_STEPS=(5 10 25 50 100 250)
 
 NUM_ALGOS=${#ALGOS[@]}
 NUM_ENVS=${#ENVS[@]}
+NUM_SUBGOAL_STEPS=${#SUBGOAL_STEPS[@]}
 ALGO_IDX=$((SLURM_ARRAY_TASK_ID % NUM_ALGOS))
 ENV_IDX=$(((SLURM_ARRAY_TASK_ID / NUM_ALGOS) % NUM_ENVS))
-SEED_IDX=$((SLURM_ARRAY_TASK_ID / (NUM_ALGOS * NUM_ENVS)))
+SUBGOAL_STEP_IDX=$(((SLURM_ARRAY_TASK_ID / (NUM_ALGOS * NUM_ENVS)) % NUM_SUBGOAL_STEPS))
+SEED_IDX=$((SLURM_ARRAY_TASK_ID / (NUM_ALGOS * NUM_ENVS * NUM_SUBGOAL_STEPS)))
 
 SEED=${SEEDS[$SEED_IDX]}
 ALGO=${ALGOS[$ALGO_IDX]}
 ENV=${ENVS[$ENV_IDX]}
+SUBGOAL_STEP=${SUBGOAL_STEPS[$SUBGOAL_STEP_IDX]}
 
 source .venv/bin/activate
 
-EXTRA_ARGS=()
+EXTRA_ARGS=("--agent.subgoal_steps=${SUBGOAL_STEP}")
 
 case "$ENV" in
     cube-single-play-v0)
-        EXTRA_ARGS+=(--agent.subgoal_steps=10)
         if [ "$ALGO" = "saw" ]; then
             EXTRA_ARGS+=(--agent.kl_alpha=0.3)
         fi
         ;;
     cube-double-play-v0)
-        EXTRA_ARGS+=(--agent.subgoal_steps=10)
         if [ "$ALGO" = "saw" ]; then
             EXTRA_ARGS+=(--agent.kl_alpha=1.0)
         fi
+        ;;
+    humanoidmaze-large-navigate-v0 | humanoidmaze-giant-navigate-v0)
+        EXTRA_ARGS+=(--agent.discount=0.995)
         ;;
 esac
 
@@ -53,5 +60,5 @@ srun python main.py \
     --eval_episodes=50 \
     --agent="agents/${ALGO}.py" \
     --seed="$SEED" \
-    --run_group="${ALGO}_test_metrics" \
+    --run_group="${ALGO}_new_metrics" \
     "${EXTRA_ARGS[@]}"
