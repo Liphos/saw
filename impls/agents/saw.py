9854e419c0d1f9b694e567dd12b3476bb46accc6
Guillaume Damien Nicolas Levy <glevy@meta.com>
2026-09-06 21:31:45 +0200

    Revert "feat: !BC change adv to correct computation"
    This reverts commit f85c811939362cf66c8e4ae2baaaf1792e6991df.

--- impls/agents/saw.py
+++ impls/agents/saw.py
@@ -64,8 +64,7 @@ class SAWAgent(flax.struct.PyTreeNode):
         nv1, nv2 = self.network.select('value')(batch['next_observations'], batch['low_actor_goals'])
         v = (v1 + v2) / 2
         nv = (nv1 + nv2) / 2
-        # Discounted one-step advantage: gamma * V(s_{t+1}, g) - V(s_t, g).
-        adv = self.config['discount'] * nv - v
+        adv = nv - v
 
         exp_a = jnp.exp(adv * self.config['low_alpha'])
         clip_pct = (exp_a > 100.0).mean() * 100.0
@@ -101,8 +100,7 @@ class SAWAgent(flax.struct.PyTreeNode):
         nv1, nv2 = self.network.select('value')(batch['next_observations'], batch['high_actor_goals'])
         v = (v1 + v2) / 2
         nv = (nv1 + nv2) / 2
-        # Discounted one-step advantage: gamma * V(s_{t+1}, g) - V(s_t, g).
-        adv = self.config['discount'] * nv - v
+        adv = nv - v
 
         exp_a = jnp.exp(adv * self.config['high_alpha'])
         clip_pct = (exp_a > 100.0).mean() * 100.0
@@ -136,10 +134,7 @@ class SAWAgent(flax.struct.PyTreeNode):
         wv1, wv2 = self.network.select('value')(batch['high_actor_targets'], batch['high_actor_goals'])
         v = (v1 + v2) / 2
         wv = (wv1 + wv2) / 2
-        # Discounted k-step advantage: gamma^k * V(s_{t+k}, g) - V(s_t, g). The realized horizon k is per-sample, as
-        # the waypoint target is clipped to the goal (or the trajectory end).
-        discount_k = self.config['discount'] ** batch['high_actor_target_dists']
-        wadv = discount_k * wv - v
+        wadv = wv - v
 
         exp_w = jnp.exp(wadv * self.config['kl_alpha'])
         clip_pct = (exp_w > 100.0).mean() * 100.0
