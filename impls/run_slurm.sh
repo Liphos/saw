167e48a36907971209b11febd09c04b85a15ae92
Guillaume Damien Nicolas Levy <glevy@meta.com>
2026-09-12 19:50:14 +0200

    run: sweep on alphas

--- impls/run_slurm.sh
+++ impls/run_slurm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=norm_proxy
-#SBATCH --output=logs/norm_proxy_%A_%a.out
-#SBATCH --error=logs/norm_proxy_%A_%a.err
-#SBATCH --array=0-479  # 2 algos * 8 envs * 6 subgoal_steps * 5 seeds
+#SBATCH --job-name=sweep_alpha_norm
+#SBATCH --output=logs/sweep_alpha_norm_%A_%a.out
+#SBATCH --error=logs/sweep_alpha_norm_%A_%a.err
+#SBATCH --array=0-599  # 2 algos * 2 envs * 6 subgoal_steps * 5 seeds * 5 alphas
 #SBATCH --time=08:00:00
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:volta:1
@@ -13,54 +13,45 @@ unset SLURM_CPU_BIND
 SEEDS=(3917 3502 8948 9460 4729)
 ALGOS=("hiql" "saw")
 ENVS=(
-    "pointmaze-giant-navigate-v0"
-    "pointmaze-large-navigate-v0"
     "antmaze-giant-navigate-v0"
     "antmaze-large-navigate-v0"
-    "cube-single-play-v0"
-    "cube-double-play-v0"
-    "humanoidmaze-large-navigate-v0"
-    "humanoidmaze-giant-navigate-v0"
 )
 SUBGOAL_STEPS=(5 10 25 50 100 250)
+HIGH_ALPHAS=(0.05 0.1 0.5 1 5)
 
 NUM_ALGOS=${#ALGOS[@]}
 NUM_ENVS=${#ENVS[@]}
 NUM_SUBGOAL_STEPS=${#SUBGOAL_STEPS[@]}
+NUM_HIGH_ALPHAS=${#HIGH_ALPHAS[@]}
 ALGO_IDX=$((SLURM_ARRAY_TASK_ID % NUM_ALGOS))
 ENV_IDX=$(((SLURM_ARRAY_TASK_ID / NUM_ALGOS) % NUM_ENVS))
 SUBGOAL_STEP_IDX=$(((SLURM_ARRAY_TASK_ID / (NUM_ALGOS * NUM_ENVS)) % NUM_SUBGOAL_STEPS))
-SEED_IDX=$((SLURM_ARRAY_TASK_ID / (NUM_ALGOS * NUM_ENVS * NUM_SUBGOAL_STEPS)))
+HIGH_ALPHA_IDX=$((SLURM_ARRAY_TASK_ID / (NUM_ALGOS * NUM_ENVS * NUM_SUBGOAL_STEPS)% NUM_HIGH_ALPHAS))
+SEED_IDX=$((SLURM_ARRAY_TASK_ID / (NUM_ALGOS * NUM_ENVS * NUM_SUBGOAL_STEPS * NUM_HIGH_ALPHAS)))
 
 SEED=${SEEDS[$SEED_IDX]}
 ALGO=${ALGOS[$ALGO_IDX]}
 ENV=${ENVS[$ENV_IDX]}
 SUBGOAL_STEP=${SUBGOAL_STEPS[$SUBGOAL_STEP_IDX]}
+HIGH_ALPHA=${HIGH_ALPHAS[$HIGH_ALPHA_IDX]}
 
 source .venv/bin/activate
 
 EXTRA_ARGS=("--agent.subgoal_steps=${SUBGOAL_STEP}")
 
-case "$ENV" in
-    cube-single-play-v0)
-        if [ "$ALGO" = "saw" ]; then
-            EXTRA_ARGS+=(--agent.kl_alpha=0.3)
-        fi
-        ;;
-    cube-double-play-v0)
-        if [ "$ALGO" = "saw" ]; then
-            EXTRA_ARGS+=(--agent.kl_alpha=1.0)
-        fi
-        ;;
-    humanoidmaze-large-navigate-v0 | humanoidmaze-giant-navigate-v0)
-        EXTRA_ARGS+=(--agent.discount=0.995)
-        ;;
-esac
+if [ "$ALGO" = "saw" ]; then
+    EXTRA_ARGS+=("--agent.kl_alpha=${HIGH_ALPHA}")
+fi
+;;
+if [ "$ALGO" = "hiql" ]; then
+    EXTRA_ARGS+=("--agent.high_alpha=${HIGH_ALPHA}")
+fi
+;;
 
 srun python main.py \
     --env_name="$ENV" \
     --eval_episodes=50 \
     --agent="agents/${ALGO}.py" \
     --seed="$SEED" \
-    --run_group="${ALGO}_norm_proxy" \
+    --run_group="${ALGO}_sweep_alpha_norm" \
     "${EXTRA_ARGS[@]}"
