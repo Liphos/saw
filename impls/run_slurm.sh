ec0232acc8fbfe48df344253a4abe49255c0ed3d
glevy <glevy@meta.com>
2026-09-18 13:03:35 +0000

    run: sweep alpha

--- impls/run_slurm.sh
+++ impls/run_slurm.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=reward_at_goal
 #SBATCH --output=logs/reward_at_goal_%A_%a.out
 #SBATCH --error=logs/reward_at_goal_%A_%a.err
-#SBATCH --array=0-179  # 1 algos * 6 envs * 6  subgoal_steps * 5 seeds
+#SBATCH --array=0-199  # 1 algos * 2 envs * 4 subgoal_steps * 5 alphas * 5 seeds 
 #SBATCH --time=08:00:00
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:volta:1
@@ -15,25 +15,25 @@ ALGOS=("hiql")
 ENVS=(
     "antmaze-giant-navigate-v0"
     "antmaze-large-navigate-v0"
-    "cube-single-play-v0"
-    "cube-double-play-v0"
-    "humanoidmaze-large-navigate-v0"
-    "humanoidmaze-giant-navigate-v0"
 )
-SUBGOAL_STEPS=(5 10 25 50 100 250)
+SUBGOAL_STEPS=(10 25 50 100)
+HIGH_ALPHAS=(0.03 0.1 0.3 1 5)
 
 NUM_ALGOS=${#ALGOS[@]}
 NUM_ENVS=${#ENVS[@]}
 NUM_SUBGOAL_STEPS=${#SUBGOAL_STEPS[@]}
+NUM_HIGH_ALPHAS=${#HIGH_ALPHAS[@]}
 ALGO_IDX=$((SLURM_ARRAY_TASK_ID % NUM_ALGOS))
 ENV_IDX=$(((SLURM_ARRAY_TASK_ID / NUM_ALGOS) % NUM_ENVS))
 SUBGOAL_STEP_IDX=$(((SLURM_ARRAY_TASK_ID / (NUM_ALGOS * NUM_ENVS)) % NUM_SUBGOAL_STEPS))
-SEED_IDX=$((SLURM_ARRAY_TASK_ID / (NUM_ALGOS * NUM_ENVS * NUM_SUBGOAL_STEPS)))
+HIGH_ALPHA_IDX=$(((SLURM_ARRAY_TASK_ID / (NUM_ALGOS * NUM_ENVS * NUM_SUBGOAL_STEPS)) % NUM_HIGH_ALPHAS))
+SEED_IDX=$((SLURM_ARRAY_TASK_ID / (NUM_ALGOS * NUM_ENVS * NUM_SUBGOAL_STEPS * NUM_HIGH_ALPHAS)))
 
 SEED=${SEEDS[$SEED_IDX]}
 ALGO=${ALGOS[$ALGO_IDX]}
 ENV=${ENVS[$ENV_IDX]}
 SUBGOAL_STEP=${SUBGOAL_STEPS[$SUBGOAL_STEP_IDX]}
+HIGH_ALPHA=${HIGH_ALPHAS[$HIGH_ALPHA_IDX]}
 
 source .venv/bin/activate
 
@@ -41,6 +41,7 @@ EXTRA_ARGS=(
     "--agent.subgoal_steps=${SUBGOAL_STEP}"
     "--agent.compute_actor_rewards=True"
     "--agent.gc_negative=False"
+    "--agent.high_alpha=${HIGH_ALPHA}"
 )
 
 case "$ENV" in
